2a3b78c53f9887c2f9afd816d0dbadfae1c7d499
konstantinvolkov <titanin88@gmail.com>
2022-06-20 01:51:01 +0300

    dv

--- src/HomeWorkRead.java
+++ src/HomeWorkRead.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -5,13 +6,12 @@ public class HomeWorkRead {
     public static void main(String[] args) throws IOException {
 
         FileReader fileReader = new FileReader("/Users/konstantinvolkov/Desktop/java/dz/out/resource/my_first_file.txt");
-        String rezult = "";
-        while (fileReader.ready()){
-            int readtxt = fileReader.read();
-            rezult += (char) readtxt;
-
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        while (bufferedReader.ready()){
+            String readLine = bufferedReader.readLine();
+            System.out.println(readLine);
         }
-        System.out.println(rezult);
+
         fileReader.close();
     }
 }
