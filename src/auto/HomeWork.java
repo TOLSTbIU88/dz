f5e7a2c6d7380ffd044b35e2662537320bf55731
konstantinvolkov <titanin88@gmail.com>
2022-06-19 21:15:45 +0300

    dv

--- src/auto/HomeWork.java
+++ src/auto/HomeWork.java
@@ -2,15 +2,17 @@ package auto;
 
 public class HomeWork {
     public static void main(String[] args) {
-        Car granta = new Lada();
-        Car corolla = new Toyota();
-        Car[] cars = {granta,corolla};
+        Lada granta = new Lada();
+        Toyota corolla = new Toyota();
         granta.startMoving();
         granta.stop();
         granta.turnLight();
-        corolla.turnLight();
-        corolla.stop();
+        granta.stop();
         corolla.startMoving();
+        corolla.stop();
+        corolla.turnLight();
+        corolla.turnMuzic();
+
 
 
     }
