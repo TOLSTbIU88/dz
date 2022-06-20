187dcff8be6b5e9b9ac57fa88bdf0b75e32dde91
konstantinvolkov <titanin88@gmail.com>
2022-06-20 13:24:03 +0300

    dv

--- src/HomeWorkRead.java
+++ src/HomeWorkRead.java
@@ -7,14 +7,15 @@ public class HomeWorkRead {
 
         FileReader fileReader = new FileReader("/Users/konstantinvolkov/Desktop/java/dz/out/resource/my_first_file.txt");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String str = bufferedReader.readLine();
-        String rezult = bufferedReader + str;
+        String rezult = "";
 
-        while (bufferedReader.ready()) {
-           str = bufferedReader.readLine();
+        while (bufferedReader.ready()){
+            String str = bufferedReader.readLine();
+            rezult += str;
+
 
         }
-        System.out.println(rezult + str);
+        System.out.println(rezult);
 
         fileReader.close();
     }
