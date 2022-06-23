0413b7eea68a454457ca686077f559f96d550254
konstantinvolkov <titanin88@gmail.com>
2022-06-23 11:22:19 +0300

    dv

--- src/auto/Financialrecord.java
+++ src/auto/Financialrecord.java
@@ -10,21 +10,5 @@ public class Financialrecord {
         this.outcomes = outcomes;
     }
 
-    public int getIncomes() {
-        return incomes;
-    }
-
-    public void setIncomes(int incomes) {
-        this.incomes = incomes;
-    }
-
-    public int getOutcomes() {
-        return outcomes;
-    }
-
-    public void setOutcomes(int outcomes) {
-        this.outcomes = outcomes;
-    }
-
     public String getInfo (){return this.incomes + "" + this.outcomes;}
 }
