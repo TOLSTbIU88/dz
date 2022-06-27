404a8152d8af4aba4b87fb4ab2fe8ae9e1b96bd5
konstantinvolkov <titanin88@gmail.com>
2022-06-27 12:17:29 +0300

    dv

--- src/lesson_7/HomeWork4.java
+++ src/lesson_7/HomeWork4.java
@@ -3,5 +3,12 @@ package lesson_7;
 public class HomeWork4 {
     public static void main(String[] args) {
 
+        Plane plane = new Plane();
+        Duck duck = new Duck();
+
+        Fligths [] flys = {plane,duck};
+        for (Fligths fligths: flys){
+            fligths.fly();
+        }
     }
 }
