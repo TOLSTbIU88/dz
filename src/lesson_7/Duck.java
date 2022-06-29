a971aa3e60bb93aee2871f95a92b5c7b56c80fc0
konstantinvolkov <titanin88@gmail.com>
2022-06-29 11:41:04 +0300

    dv

--- src/lesson_7/Duck.java
+++ src/lesson_7/Duck.java
@@ -6,12 +6,12 @@ public class Duck extends FlyException implements Fligths {
     public Duck(boolean isInjured) {
         this.isInjured = isInjured;
     }
-
+    public boolean isInjured() {
+        return isInjured;
+    }
     public void setInjured(boolean injured) {
         isInjured = injured;
-    }
-
-    public void fly(){
+    }public void fly(){
         System.out.println("Утка летит");
     }
 
