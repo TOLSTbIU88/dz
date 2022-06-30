d380f1f2d26182756eba82b396c1968ed26b0493
konstantinvolkov <titanin88@gmail.com>
2022-06-30 17:39:01 +0300

    dv

--- src/lesson_7/FlyException.java
+++ src/lesson_7/FlyException.java
@@ -5,10 +5,9 @@ public class FlyException extends Exception {
     private boolean isInjured;
     private int countPassengers;
 
-    public FlyException(String isInjured, boolean countPassengers) {
+    public FlyException(boolean isInjured, int countPassengers) {
         this.isInjured = isInjured;
         this.countPassengers = countPassengers;
     }
-
 
 }
